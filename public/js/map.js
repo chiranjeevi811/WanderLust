06f65fd8ec9368bdb28f83f5c801621fd86ee962
Chiranjeevi Adithya <chiranjeevi210606@gmail.com>
2026-07-08 17:35:17 +0530

    check maptoken

--- public/js/map.js
+++ public/js/map.js
@@ -1,21 +1,19 @@
+console.log(listing.geometry.coordinates);
+console.log(mapToken);
 
-
-
-  const map = new maplibregl.Map({
-    container: "map", // same container ID
-    style: `https://api.maptiler.com/maps/streets/style.json?key=${mapToken}`, // MapTiler style
-    center:listing.geometry.coordinates, // same starting position [lng, lat]
-    zoom: 9, // starting zoom
-  });
+const map = new maplibregl.Map({
+  container: "map", // same container ID
+  style: `https://api.maptiler.com/maps/streets/style.json?key=${mapToken}`, // MapTiler style
+  center: listing.geometry.coordinates, // same starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
 
 const marker = new maplibregl.Marker({ color: "red" })
   .setLngLat(listing.geometry.coordinates) // [lng, lat]
   .setPopup(
     new maplibregl.Popup({ offset: 25 }).setHTML(
       `<h4>${listing.title}</h4>
-       <p>Exact location will be provided after booking</p>`
-    )
+       <p>Exact location will be provided after booking</p>`,
+    ),
   )
   .addTo(map);
-
-
